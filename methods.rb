e2d16e56cb411c54481564cbf552addc9c10c592
Sam Gubernikoff <sgubernikoff@gmail.com>
2022-03-13 21:08:47 -0400

    commit tree b6553a9bc34ad1f72204fdec0cf849950290d063

--- methods.rb
+++ methods.rb
@@ -1,1 +1,26 @@
 # Your code here!
+def greet_programmer 
+    puts "Hello, programmer!"
+end
+
+def greet param
+    puts "Hello, #{param}!"
+end
+    
+# greet(Naureen)
+
+def greet_with_default param = "programmer"
+    puts "Hello, #{param}!"
+end
+
+def add num1, num2 
+    num1 + num2
+end
+
+def halve num1
+    
+    if num1.class != Integer
+         return nil
+        end
+        num1 / 2
+end
